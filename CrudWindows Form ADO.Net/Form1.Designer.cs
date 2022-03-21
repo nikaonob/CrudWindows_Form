5df010a4d653079a2b3e0147cd160b2e62d77db1
Nikaonob <nikaonob91@gmail.comgit config --global user.email nikaonob91@gmail.com>
2022-03-21 19:00:16 -0300

    inicio del programa

---
+++ CrudWindows Form ADO.Net/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace CrudWindows_Form_ADO.Net
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgPeople = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgPeople)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgPeople
+            // 
+            this.dtgPeople.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgPeople.Location = new System.Drawing.Point(48, 58);
+            this.dtgPeople.Name = "dtgPeople";
+            this.dtgPeople.Size = new System.Drawing.Size(424, 287);
+            this.dtgPeople.TabIndex = 0;
+            this.dtgPeople.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 357);
+            this.Controls.Add(this.dtgPeople);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgPeople)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgPeople;
+    }
+}
+
---
+++ CrudWindows Form ADO.Net/People.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CrudWindows_Form_ADO.Net
+{
+    public  class PeopleBD
+    {
+        private string connectionString = "" +
+            "Data Source=localhost;Initial Catalog=CrudWindowsForm;" +
+            "User=sa;Password=b4ut1n0b";
+
+        public bool OK()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public List<People> Get()
+        {
+            List<People> people = new List<People>();
+                string query = "select name,age,id from people";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand comando = new SqlCommand(query,connection);
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = comando.ExecuteReader();
+
+                    while(reader.Read())
+                    {
+                        People oPeople = new People();
+                        oPeople.Name = reader.GetString(0);
+                        oPeople.Age =reader.GetInt32(1);
+                        oPeople.ID = reader.GetInt32(2);
+                        people.Add(oPeople);
+                    }
+                    reader.Close();
+                    connection.Close();
+                }
+                catch(Exception ex)
+                {
+                    throw new Exception("HAY UN ERROR" + ex.Message);
+                }
+                
+            }
+            return people;
+        }
+        
+    }
+    public class People
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int ID { get; set; }
+    }
+
+}
